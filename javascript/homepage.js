00165d0380a44d7c96322f405991b712fe846cee
aglathe <adrianglathede@gmail.com>
2023-11-23 00:22:22 +0100

    zur arbeit auf laptop

---
+++ javascript/homepage.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/xxeS9xpmTA5yHp9q/scene.splinecode');
